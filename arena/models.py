e84373c15735f222cda9cf8547559bd1183200ff
Ananth95 <anantha0707@gmail.com>
2016-01-24 00:32:30 +0530

    Restructured files
    Todo
    - Destination Folder and file name

--- arena/models.py
+++ arena/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from . import forms
 
 class Document(models.Model):
-    docfile = models.FileField(upload_to='documents/%Y/%m/%d')
+	foldername = 'bots'
+	docfile = models.FileField(upload_to=foldername)
--- arena/views.py
+++ arena/views.py
@@ -1,18 +1,11 @@
 from django.shortcuts import render
-from .models import Document
-from .forms import DocumentForm
-
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-
 from django.views.decorators.csrf import csrf_exempt
 
-
+from .models import Document
+from .forms import DocumentForm
 # Create your views here.
-@csrf_exempt
-def home(request):
-    return render(request,'list.html')
-
 @csrf_exempt
 def success(request):
     return render(request,'success.html')
@@ -22,7 +15,7 @@ def slf(request):
     return render(request,'slf.html')
 
 @csrf_exempt
-def list(request):
+def redirect(request):
 
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
@@ -30,18 +23,11 @@ def list(request):
             newdoc = Document(docfile=request.FILES['docfile'])
             newdoc.save()
 
-            return render_to_response(
-            'success.html',
-            context_instance=RequestContext(request))
+            return render_to_response('success.html',context_instance=RequestContext(request))
 
         else:
-            return render_to_response(
-            'slf.html',
-            context_instance=RequestContext(request))
+            return render_to_response('fnv.html',context_instance=RequestContext(request))
 
     else:
         form = DocumentForm()
-
-    return render_to_response(
-        'failure.html',
-        context_instance=RequestContext(request))
+    return render_to_response('failure.html',context_instance=RequestContext(request))
